78706d41e6eb1fdcd9fff42f4d73c6dbf6ebb8f3
unknown <asorenson01@hotmail.com>
2021-04-15 13:56:56 -0400

    all base classes built and imported

---
+++ venv/dinosaur.py
@@ -0,0 +1,8 @@
+class Dinosaur:
+    def __init__(self, type, attack_power):
+        self.type = ''
+        self.energy = 0
+        self.attack_power = 0
+        self.int = 0
+
+    def attack(robot): #void
